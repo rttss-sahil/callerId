790b32a5ccc5d89679c74d5fcd8dcfb63b1ea3c5
Sahil <sahil.rathee@infortts.com>
2024-04-10 12:04:06 +0530

    minor fix: added spam_count for searchbyNumber

--- server/src/services/searchService.ts
+++ server/src/services/searchService.ts
@@ -27,7 +27,12 @@ export default class SearchService {
         try {
             let result: QueryResult;
             result = await opts.context.pgClient.executeQuery(`
-                SELECT id, name, phone_number, 'User' AS source, spam_count FROM "User" WHERE phone_number = '${args.phone}';
+                SELECT u.id, u.name, u.phone_number, 'User' AS source, COUNT(s.id) AS spam_count
+                FROM "User" u
+                LEFT JOIN global_users gu ON u.phone_number = gu.phone_number AND gu.source = 'User'
+                LEFT JOIN Spam s ON u.phone_number = s.phone_number
+                WHERE u.phone_number = '${args.phone}'
+                GROUP BY u.id, u.name, u.phone_number;
             `);
             if (result.rows.length > 0) {
                 return result.rows;
